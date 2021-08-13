cbf976c597d26f5128b63454457e7ca6c3150ed3
Karen Marting <karenannemarting@yahoo.com>
2021-08-13 16:36:03 -0700

    models

---
+++ models/index.js
@@ -0,0 +1,41 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// do we need these here?
+const seedProducts = require('../seeds/product-seeds');
+const seedTags = require('../seeds/tag-seeds');
+
+// Products belongsTo Category
+
+Product.belongsToOne(Category, {
+  foreignKey: 'category_id',
+});
+// Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'category_id',
+})
+
+// Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
